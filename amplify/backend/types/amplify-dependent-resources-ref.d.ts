aaef0a89e66d55770b932d622b5611bb4eede163
shivanitrivedi123 <shivani.trivedi@gtcsys.com>
2024-01-15 16:09:54 +0530

    feat: # - Changes In the todoapi

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "demoproject": {
+    "todoapi": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import config from "./amplifyconfiguration.json";
+import { createTodo, updateTodo, deleteTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+Amplify.configure(config);
+
+const client = generateClient();
 
 function App() {
+  useEffect(() => {
+    async function createTodoItem() {
+      const result = await client.graphql({
+        query: createTodo,
+        variables: {
+          input: {
+            name: "My first todo!",
+            description: "Hello World",
+          },
+        },
+      });
+      console.log(result, "resultsss");
+    }
+    createTodoItem();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       Just Start with learning AWS-Amplify
-        </p>
+        <p>Just Start with learning AWS-Amplify</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hello Amplify
+          Hello Amplify
         </a>
       </header>
     </div>
