50ab2824abecb1f1e3ef3376d815d3c576671919
shivanitrivedi123 <shivani.trivedi@gtcsys.com>
2024-01-22 16:53:20 +0530

    feat: # - Changes In The Post Component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,53 +1,55 @@
 import React from "react";
-import { Amplify } from "aws-amplify";
-import config from "../amplifyconfiguration.json";
-import { generateClient } from "aws-amplify/api";
-import { createPost, createComment } from '../graphql/mutations';
-import { getPost } from '../graphql/queries';
+// import { Amplify } from "aws-amplify";
+// import config from "../amplifyconfiguration.json";
+// import { generateClient } from "aws-amplify/api";
+// import { createPost, createComment } from '../graphql/mutations';
+// import { getPost } from '../graphql/queries';
 
-Amplify.configure(config);
-const client = generateClient();
+// Amplify.configure(config);
+// const client = generateClient();
 
-const Post = () => {
-  const handleTaskAdd = async () => {
-    try {
+// const Post = () => {
+//   const handleTaskAdd = async () => {
+//     try {
        
-        const result1 = await client.graphql({
-          query: createPost,
-          variables: {
-            input: { title: 'Hello World!!' }
-          }
-        });
-        const post = result1.data.createPost;
-        console.log(post,"postt")
-        // create comment
-        await client.graphql({
-          query: createComment,
-          variables: {
-            input: { content: 'Hi!', postID: post.id }
-          }
-        });
+//         const result1 = await client.graphql({
+//           query: createPost,
+//           variables: {
+//             input: { title: 'Hello World!!' }
+//           }
+//         });
+//         const post = result1.data.createPost;
+//         console.log(post,"postt")
+//         // create comment
+//         await client.graphql({
+//           query: createComment,
+//           variables: {
+//             input: { content: 'Hi!', postID: post.id }
+//           }
+//         });
         
-        // get post
-        const result = await client.graphql({
-          query: getPost,
-          variables: { id: post.id }
-        });
+//         // get post
+//         const result = await client.graphql({
+//           query: getPost,
+//           variables: { id: post.id }
+//         });
         
-        const postWithComments = result.data.createPost;
-        console.log(postWithComments,"postWithCommentspostWithComments")
-        const postComments = result.data.createComment;
-        console.log(postComments,"postCommentspostComments+++++++++++++++++++++")
-    } catch (error) {
-      console.error("Error", error);
-    }
-  };
+//         const postWithComments = result.data.createPost;
+//         console.log(postWithComments,"postWithCommentspostWithComments")
+//         const postComments = result.data.createComment;
+//         console.log(postComments,"postCommentspostComments+++++++++++++++++++++")
+//     } catch (error) {
+//       console.error("Error", error);
+//     }
+//   };
 
-  return (
-    <div>
-      <button onClick={handleTaskAdd}>handleTaskAdd</button>
-    </div>
-  );
-};
-
+//   return (
+//     <div>
+//       <button onClick={handleTaskAdd}>handleTaskAdd</button>
+//     </div>
+//   );
+// };
+const Post =()=>{
+    return <div>Post</div>
+}
 export default Post;
